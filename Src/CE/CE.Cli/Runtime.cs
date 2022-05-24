3f8c1b9e80892b250574c0f15670861276f2d2cb
Johan Potgieter <johan.ccs@gmail.com>
2022-05-24 14:21:07 +0200

    Adding Orders with status In_Progress. Add menu support. Add classes to format and display the orders detail.

--- Src/CE/CE.Cli/Runtime.cs
+++ Src/CE/CE.Cli/Runtime.cs
@@ -26,22 +26,87 @@ namespace CE.Cli
 
         public async Task Run()
         {
+            var answer = await DisplayMenu();
+
+            await DisplayByMenuItem(answer);
+        }
+
+        private async Task<int> DisplayMenu()
+        {
+            Console.WriteLine("Please select an action from the menu");
+            Console.WriteLine("-------------------------------------");
+
+            Console.WriteLine("1. Orders By In progress status");
+            Console.WriteLine("2. Top five products sold");
+            Console.WriteLine("3. Update product quantity");
+
+            Console.WriteLine("");
+
+            Console.Write("Answer: ");
+            
+            return await Task.FromResult(Convert.ToInt32(Console.ReadLine()));
+        }
+
+        private async Task DisplayByMenuItem(int answer)
+        {
+            switch(answer)
+            {
+                case 1:
+                     PrintAndClearEmptyLine();
+                     await DisplayOrdersInProgress();
+                     break;
+
+                case 2:
+                    await DisplayTopFiveProducts();
+                    break;
+
+                case 3:
+                    await UpdateOrderQuantity();
+                    break;
+
+                default:
+                    Console.Clear();
+                    await DisplayMenu();
+                    return;
+
+            }
+
+            Console.WriteLine("");
+            Console.Write("Press 9 to go to Main Menu: ");
+            var ans = Convert.ToInt32(Console.ReadLine());
+
+            if (ans == 9)
+            {
+                Console.Clear();
+                await DisplayMenu();
+            }
+        }
+
+        private Task UpdateOrderQuantity()
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task DisplayTopFiveProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task DisplayOrdersInProgress()
+        {
             var results = await _orderService.GetAllByInProgressStatus();
 
             if (results.Count > 0)
             {
-                var builder = new StringBuilder();
-                builder.AppendLine("Id | Order Date | ");
-
-                foreach (var result in results)
-                {
-                    builder.AppendLine($"{nameof(result.Id)}: {result.Id} | ");
-
-                }
-
-                Console.WriteLine(builder.ToString());
-                Console.Read();
+                DisplayFormattedData.WriteHeader();
+                DisplayFormattedData.WriteBody(results);
             }
+        }
+
+        private void PrintAndClearEmptyLine()
+        {
+            Console.Clear();
+            Console.WriteLine("");
         }
 
         #endregion
